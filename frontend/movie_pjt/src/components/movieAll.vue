e9a4e0dd9497809106840a0536d9620ed6c2d9ce
JSG <jeongsang-geun@Toproot.local>
2021-05-20 17:39:45 +0900

    initial commit

---
+++ frontend/movie_pjt/src/components/movieAll.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'movieAll',
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/movie_pjt/src/components/photoChoice.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'photoChoice'
+}
+</script>
+
+<style>
+
+</style>
