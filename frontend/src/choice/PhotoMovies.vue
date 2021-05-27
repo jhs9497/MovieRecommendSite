8ffcf4cc5b2680af716d6f7b34c8c96c99be90de
JSG <jeongsang-geun@Toproot.local>
2021-05-27 17:18:18 +0900

    추천알고리즘 기능완성 FV1.6

--- frontend/src/choice/PhotoMovies.vue
+++ frontend/src/choice/PhotoMovies.vue
@@ -84,55 +84,135 @@
       </v-row>
     </section>
     <!-- banner -->
-    <!-- 추천영화 -->
-    <v-card>
-      <v-container fluid>
-        <v-row>
-          <v-col
-            v-for="movie in moives"
-            :key="movie"
-            cols="12"
-            md="3"
-          >
-            <v-img
-              v-bind:src="movie.poster_path"
-              aspect-ratio="1"
-              height="500px"
-            ></v-img>
-          </v-col>
-        </v-row>
-      </v-container>
-      <!-- </v-tabs> -->
-    </v-card>
-    <!-- 추천영화 -->
 
+    <!-- 추천영화 -->
+    <section>
+      <v-card
+        class="d-flex justify-space-around mb-6"
+        :color="$vuetify.theme.dark ? 'grey darken-3' : 'white'"
+        flat
+        tile
+      >
+        <v-card
+          v-for="movie in randomMovies"
+          :key="movie"
+          class="pa-2"
+          outlined
+          tile
+        >
+          <!-- 포스터 -->
+          <v-card
+            :loading="loading"
+            class="mx-auto my-12"
+            max-width="374"
+          >
+            <template slot="progress">
+              <v-progress-linear
+                color="deep-purple"
+                height="10"
+                indeterminate
+              ></v-progress-linear>
+            </template>
+
+            <v-img
+              height="250"
+              :src="movie.poster_path"
+            ></v-img>
+
+            <v-card-title>Cafe Badilico</v-card-title>
+
+            <v-card-text>
+              <v-row
+                align="center"
+                class="mx-0"
+              >
+                <v-rating
+                  :value="4.5"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+                ></v-rating>
+
+                <div class="grey--text ms-4">
+                  4.5 (413)
+                </div>
+              </v-row>
+
+              <div class="my-4 subtitle-1">
+                $ • Italian, Cafe
+              </div>
+
+            </v-card-text>
+
+            <v-divider class="mx-4"></v-divider>
+
+            <v-card-title>Tonight's availability</v-card-title>
+
+            <v-card-actions>
+              <v-btn
+                color="deep-purple lighten-2"
+                text
+                @click="reserve"
+              >
+                Reserve
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+          <!-- 포스터 -->
+        </v-card>
+      </v-card>
+    </section>
+    <!-- 추천영화 -->
   </div>
 </template>
 
 <script>
+import _ from 'lodash'
+
 export default {
   name: 'PhotoMovies',
   props: {
-    photo_url:{
-    type: String,
-    default: ''
-    },
-    photo_theme:{
+    photo_url: {
       type: String,
-      default: ''
+      default : '',
+    },
+    photo_theme: {
+      type: String,
+      default : '',
+    },
+    photo_genreid: {
+      type: String,
+      default: '',
     },
   },
   data () {
     return {
       movies: [],
+      randomNums: [],
+      randomMovies: [],
     }
   },
   async created() {
-    const url = 'http://127.0.0.1:8000/api/v1/movie/' // 장고의 서버주소
-    const response = await this.axios.get(url)
-    this.movies = response.data
-  },
-  method: {
+    for (let i=0; i < this.photo_genreid.length; i++) {
+      const id = String(i)
+      const genreId = this.photo_genreid[id]
+      const url = `http://127.0.0.1:8000/api/v1/movie/genre/${genreId}/` // 장고의 서버주소
+      const response = await this.axios.get(url)
+      for (let i=0; i < response.data.length; i++){
+        this.movies.push(response.data[i])
+    }
+  }
+    const randomNumbers = _.range(1, this.movies.length) // 1부터 영화개수 중 랜덤 숫자 추출
+    this.randomNums = _.sampleSize(randomNumbers, this.photo_genreid.length) // 장르 개수만큼 랜덤숫자 뽑기
+    // 뽑은 랜덤숫자 순서에 해당하는 영화 뽑아서 저장하기
+    for (let i=0; i < this.randomNums.length; i++){
+      const id = String(i)
+      const randomnumber = this.randomNums[id]
+      this.randomMovies.push(this.movies[randomnumber])
+    }
+    console.log(this.randomNums)
   }
 }
 </script>
